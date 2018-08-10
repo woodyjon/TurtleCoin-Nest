fdac719364aaa2fcc86357bb1a7e047fd49762a4
Unknown <woodyjon@gmail.com>
2018-08-10 14:42:09 +0200

    turtle-services -> turtle-service

--- walletdmanager/constants.go
+++ walletdmanager/constants.go
@@ -6,11 +6,11 @@ const (
 	// DefaultTransferMixin is the default mixin
 	DefaultTransferMixin = 7
 
-	logWalletdCurrentSessionFilename     = "turtle-services-session.log"
-	logWalletdAllSessionsFilename        = "turtle-services.log"
+	logWalletdCurrentSessionFilename     = "turtle-service-session.log"
+	logWalletdAllSessionsFilename        = "turtle-service.log"
 	logTurtleCoindCurrentSessionFilename = "Turtlecoind-session.log"
 	logTurtleCoindAllSessionsFilename    = "TurtleCoind.log"
 	walletdLogLevel                      = "3" // should be at least 3 as I use some logs messages to confirm creation of wallet
-	walletdCommandName                   = "turtle-services"
+	walletdCommandName                   = "turtle-service"
 	turtlecoindCommandName               = "TurtleCoind"
 )
