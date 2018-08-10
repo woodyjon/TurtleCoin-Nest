0396b660fb8632e23ea961e27b8f0d49cff066ad
Unknown <woodyjon@gmail.com>
2018-08-10 17:26:38 +0200

    typo

--- walletdmanager/constants.go
+++ walletdmanager/constants.go
@@ -8,7 +8,7 @@ const (
 
 	logWalletdCurrentSessionFilename     = "turtle-service-session.log"
 	logWalletdAllSessionsFilename        = "turtle-service.log"
-	logTurtleCoindCurrentSessionFilename = "Turtlecoind-session.log"
+	logTurtleCoindCurrentSessionFilename = "TurtleCoind-session.log"
 	logTurtleCoindAllSessionsFilename    = "TurtleCoind.log"
 	walletdLogLevel                      = "3" // should be at least 3 as I use some logs messages to confirm creation of wallet
 	walletdCommandName                   = "turtle-service"
