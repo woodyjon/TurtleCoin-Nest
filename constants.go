d722314b02117d8f5f91f6df2ee0df538aca79ae
Brandon <brandonlehmann@gmail.com>
2018-08-27 13:32:28 -0400

    Update to Version 0.33
    Bumping version to avoid confusion

--- constants.go
+++ constants.go
@@ -1,7 +1,7 @@
 package main
 
 const (
-	versionNest                 = "0.32"
+	versionNest                 = "0.33"
 	urlCryptoCompareTRTL        = "https://min-api.cryptocompare.com/data/price?fsym=TRTL&tsyms=USD"
 	defaultRemoteDaemonAddress  = "public.turtlenode.io"
 	defaultRemoteDaemonPort     = "11898"
