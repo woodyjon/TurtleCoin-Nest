493e5968e287edd238aee1c3647cb949958f2f07
Unknown <woodyjon@gmail.com>
2018-10-03 16:49:58 +0200

    save and get from db the prefered node to be selected

--- main.go
+++ main.go
@@ -246,7 +246,7 @@ func main() {
 	getAndDisplayStartInfoFromDB()
 
 	go func() {
-		getListRemoteNodes()
+		getAndDisplayListRemoteNodes()
 	}()
 
 	go func() {
@@ -347,6 +347,9 @@ func connectQMLToGOFunctions() {
 
 	qmlBridge.ConnectSelectedRemoteNode(func(index int) {
 		indexSelectedRemoteNode = index
+
+		node := remoteNodes[indexSelectedRemoteNode]
+		recordSelectedRemoteDaemonToDB(node)
 	})
 
 	qmlBridge.ConnectClickedCloseSettings(func() {
@@ -787,9 +790,16 @@ func setupDB(pathToDB string) {
 		log.Fatal("error creating table fiatConversion. err: ", err)
 	}
 
+	// table for storing custom node set in settings
 	_, err = db.Exec("CREATE TABLE IF NOT EXISTS remoteNodeInfo (id INTEGER PRIMARY KEY AUTOINCREMENT, address VARCHAR(64), port VARCHAR(64))")
 	if err != nil {
 		log.Fatal("error creating table remoteNodeInfo. err: ", err)
+	}
+
+	// table for remembering which remote node was lastly selected by the user in the list
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS selectedRemoteNode (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(64), address VARCHAR(64), port INTEGER)")
+	if err != nil {
+		log.Fatal("error creating table selectedRemoteNode. err: ", err)
 	}
 
 	_, err = db.Exec("CREATE TABLE IF NOT EXISTS savedAddresses (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(64), address VARCHAR(64), paymentID VARCHAR(64))")
@@ -901,6 +911,35 @@ func recordRemoteDaemonInfoToDB(daemonAddress string, daemonPort string) {
 	_, err = stmt.Exec(daemonAddress, daemonPort)
 	if err != nil {
 		log.Fatal("error inserting address and port of remote node into db. err: ", err)
+	}
+}
+
+func getSelectedRemoteDaemonFromDB() (daemonAddress string, daemonPort int) {
+
+	rows, err := db.Query("SELECT address, port FROM selectedRemoteNode ORDER BY id DESC LIMIT 1")
+	if err != nil {
+		log.Fatal("error querying address and port from selectedRemoteNode table. err: ", err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		err = rows.Scan(&daemonAddress, &daemonPort)
+		if err != nil {
+			log.Fatal("error reading item from selectedRemoteNode table. err: ", err)
+		}
+	}
+
+	return daemonAddress, daemonPort
+}
+
+func recordSelectedRemoteDaemonToDB(selectedNode node) {
+
+	stmt, err := db.Prepare(`INSERT INTO selectedRemoteNode(name,address,port) VALUES(?,?,?)`)
+	if err != nil {
+		log.Fatal("error preparing to insert name, address and port of selected remote node into db. err: ", err)
+	}
+	_, err = stmt.Exec(selectedNode.Name, selectedNode.URL, selectedNode.Port)
+	if err != nil {
+		log.Fatal("error inserting name, address and port of selected remote node into db. err: ", err)
 	}
 }
 
@@ -1016,15 +1055,29 @@ func requestRateTRTL() {
 	}
 }
 
-func getListRemoteNodes() {
+func getAndDisplayListRemoteNodes() {
 	remoteNodes = requestListRemoteNodes()
+
+	// to preselect the node previously selected by the user
+	addressPreferedNode, portPreferedNode := getSelectedRemoteDaemonFromDB()
+
+	preferedNodeFound := false
 
 	for index, aNode := range remoteNodes {
 		qmlBridge.AddRemoteNodeToList(aNode.URL)
 
-		if aNode.URL == defaultRemoteDaemonAddress {
-			// select defaultRemoteDaemonAddress as default in the list
+		if addressPreferedNode != "" && aNode.URL == addressPreferedNode && aNode.Port == uint64(portPreferedNode) {
 			indexSelectedRemoteNode = index
+			preferedNodeFound = true
+		}
+	}
+
+	// if user prefered node not found, select the default one in the list
+	if !preferedNodeFound {
+		for index, aNode := range remoteNodes {
+			if aNode.URL == defaultRemoteDaemonAddress {
+				indexSelectedRemoteNode = index
+			}
 		}
 	}
 
